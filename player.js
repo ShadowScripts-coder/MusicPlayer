79f7cbd3c7dff375354d2b8d20f80807247b4797
ShadowScripts-coder <122163139+ShadowScripts-coder@users.noreply.github.com>
2024-10-02 17:06:14 +0100

    add progress bar and play/pause button

--- player.js
+++ player.js
@@ -1,50 +1,49 @@
 const audioFiles = [
-    'Back In Black [ ezmp3.cc ].mp3', 
-    'Barracuda [ ezmp3.cc ].mp3',
-    'Come As You Are [ ezmp3.cc ].mp3',
-    "Don't Look Back In Anger (Remastered) [ ezmp3.cc ].mp3",
-    "Don't Stop Believin' [ ezmp3.cc ].mp3",
-    "Don't Stop Me Now (Remastered 2011) [ ezmp3.cc ].mp3",
-    'Everybody Wants To Rule The World [ ezmp3.cc ].mp3',
-    'Everything Must Go (2016 Remastered Version) [ ezmp3.cc ].mp3',
-    'Eye of the Tiger [ ezmp3.cc ].mp3',
-    'Footloose (From _Footloose_ Soundtrack) [ ezmp3.cc ].mp3',
-    'Heart-Shaped Box [ ezmp3.cc ].mp3',
-    'Highway to Hell [ ezmp3.cc ].mp3',
-    "I Love Rock 'N Roll [ ezmp3.cc ].mp3",
-    'I Want To Break Free [ ezmp3.cc ].mp3',
-    "I Was Made For Lovin' You [ ezmp3.cc ].mp3",
-    "I'm Still Standing [ ezmp3.cc ].mp3",
-    'Iron Man (2012 - Remaster) [ ezmp3.cc ].mp3',
-    "It's My Life [ ezmp3.cc ].mp3",
-    'Live Forever (Remastered) [ ezmp3.cc ].mp3',
-    "Livin' On A Prayer [ ezmp3.cc ].mp3",
-    'Paranoid (2009 Remaster) [ ezmp3.cc ].mp3',
-    'Seven Nation Army [ ezmp3.cc ].mp3',
-    'Sharp Dressed Man (2008 Remaster) [ ezmp3.cc ].mp3',
-    'Should I Stay or Should I Go (Remastered) [ ezmp3.cc ].mp3',
-    'Smells Like Teen Spirit [ ezmp3.cc ].mp3',
-    'Something In The Way [ ezmp3.cc ].mp3',
-    "Sweet Child O' Mine [ ezmp3.cc ].mp3",
-    'Sweet Dreams (Are Made of This) (Remastered) [ ezmp3.cc ].mp3',
-    'T.N.T. [ ezmp3.cc ].mp3',
-    'Take on Me [ ezmp3.cc ].mp3',
-    'The Final Countdown [ ezmp3.cc ].mp3',
-    'Thunderstruck [ ezmp3.cc ].mp3',
-    'Under Pressure (Remastered 2011) [ ezmp3.cc ].mp3',
-    'Uptown Girl [ ezmp3.cc ].mp3',
-    'Viva La Vida [ ezmp3.cc ].mp3',
-    'We Are The Champions (Remastered 2011) [ ezmp3.cc ].mp3',
-    'We Will Rock You (Remastered 2011) [ ezmp3.cc ].mp3',
-    'Wonderwall [ ezmp3.cc ].mp3',
-    'You Give Love A Bad Name [ ezmp3.cc ].mp3'
+    "Back In Black.mp3", 
+    "Barracuda.mp3", 
+    "Come As You Are.mp3", 
+    "Don't Look Back In Anger.mp3", 
+    "Don't Stop Believin'.mp3", 
+    "Don't Stop Me Now.mp3", 
+    "Everybody Wants To Rule The World.mp3", 
+    "Everything Must Go.mp3", 
+    "Eye of the Tiger.mp3", 
+    "Footloose.mp3", 
+    "Heart-Shaped Box.mp3", 
+    "Highway to Hell.mp3",
+    "I Love Rock 'N Roll.mp3",
+    "I Want To Break Free.mp3",
+    "I Was Made For Lovin' You.mp3",
+    "I'm Still Standing.mp3",
+    "Iron Man.mp3",
+    "It's My Life.mp3",
+    "Live Forever.mp3",
+    "Livin' On A Prayer.mp3",
+    "Paranoid.mp3",
+    "Seven Nation Army.mp3",
+    "Sharp Dressed Man.mp3",
+    "Should I Stay or Should I Go.mp3",
+    "Smells Like Teen Spirit.mp3",
+    "Something In The Way.mp3",
+    "Sweet Child O' Mine.mp3",
+    "Sweet Dreams (Are Made of This).mp3",
+    "T.N.T..mp3",
+    "Take on Me.mp3",
+    "The Final Countdown.mp3",
+    "Thunderstruck.mp3",
+    "Under Pressure.mp3",
+    "Uptown Girl.mp3",
+    "Viva La Vida.mp3",
+    "We Are The Champions.mp3",
+    "We Will Rock You.mp3",
+    "Wonderwall.mp3",
+    "You Give Love A Bad Name.mp3"
 ];
 
 let currentAudio = null; // To store the current audio object
 
-document.getElementById('pickMp3Button').addEventListener('click', () => {
-    playAudio();
-});
+
+
 
 // Array to keep track of the last 5 audio files played
 let previousFiles = [];
@@ -65,9 +64,9 @@ function playAudio() {
     }
 
     // Create a new audio object and play it
-    currentAudio = new Audio('./Epic Music/' + selectedFile);
-    document.getElementById("song-title").innerHTML = selectedFile.split(" [")[0];
-    document.getElementById("song-cover").setAttribute("src", './Covers/' + selectedFile.split("[")[0] + ".jpg");
+    currentAudio = new Audio('../Epic Music/' + selectedFile);
+    document.getElementById("song-title").innerHTML = selectedFile.split(".mp3")[0];
+    document.getElementById("song-cover").setAttribute("src", '../Covers/' + selectedFile.split(".mp3")[0] + ".jpg");
     currentAudio.volume = document.getElementById('volume-slider').value; // Set volume from slider
     currentAudio.play().catch(error => {
         console.error('Error playing audio:', error);
@@ -79,15 +78,51 @@ function playAudio() {
         previousFiles.shift(); // Remove the oldest file if more than 5
     }
 
+    timeUpdate();
+
     currentAudio.addEventListener('ended', () => {
         playAudio();
     });
 }
 
 
+function isPlaying(audioElement) {
+    return !audioElement.paused;
+}
+
 // Volume slider event listener
 document.getElementById('volume-slider').addEventListener('input', function() {
     if (currentAudio) {
         currentAudio.volume = this.value; // Update the volume of the current audio
+    }
+});
+
+function timeUpdate() {
+    currentAudio.addEventListener('timeupdate', () => {
+        const progress = (currentAudio.currentTime / currentAudio.duration) * 100;
+        document.getElementById('progress-bar').value = progress;
+    });
+}
+
+document.getElementById('progress-bar').addEventListener('input', function() {
+    const seekTime = (this.value / 100) * currentAudio.duration;
+    currentAudio.currentTime = seekTime;
+});
+
+document.getElementById('pauseButton').addEventListener('click', () => {
+    currentAudio.pause();
+    document.getElementById('pauseButton').style.display = "none";
+    document.getElementById('pickMp3Button').style.display = "inline";
+})
+
+document.getElementById('pickMp3Button').addEventListener('click', () => {
+    if (currentAudio == null) {
+        playAudio();
+        document.getElementById('pickMp3Button').style.display = "none";
+        document.getElementById('pauseButton').style.display = "inline";
+    } else {
+        currentAudio.play();
+        document.getElementById('pickMp3Button').style.display = "none";
+        document.getElementById('pauseButton').style.display = "inline";
     }
 });
